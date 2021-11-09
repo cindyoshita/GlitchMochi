abf412c9784fb1ac35e8f1af89556465b345931b
ReiAkio <tanaka1557@gmail.com>
2021-11-09 17:22:10 -0300

    Application kt

--- GlitchMochi/src/main/kotlin/com/glitchmochi/Application.kt
+++ GlitchMochi/src/main/kotlin/com/glitchmochi/Application.kt
@@ -1,38 +1,35 @@
-/*
+
 package com.glitchmochi
 
 import ENUMs.TYPE
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import dao.CommentDAO
+import dao.GameRecommendationDAO
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import models.Comments
+import models.GameRecommendation
 
 fun main() {
 
     embeddedServer(Netty, port = 80, host = "0.0.0.0") {
         routing{
-            get("/Comments"){
-                val commentDAO = CommentDAO()
-                commentDAO.update(Comments("Hyundai", "123", 2, 3, 1, 0))
-                var comments = commentDAO.getAll();
-                var commentsList : MutableList<Comments> = mutableListOf()
-                for (comment in comments){
-                    println(comment)
-                    commentsList.add(comment as Comments)
-                }
-                call.respondText(commentsList.toString())
-            }
             get("/ola"){
+                println("Entrou na rota ola")
                 call.respondText("ola")
             }
             get("/owner/{owner}"){
                 call.respondText("Bem vindo ${call.parameters["owner"]}")
             }
+            get("/jogo/{jogoid}"){
+                val jogoid : Int = call.parameters["jogoid"]!!.toInt()
+                val gameRecommendationDAO : GameRecommendationDAO = GameRecommendationDAO()
+                val gr = gameRecommendationDAO.getOne(jogoid)
+                call.respondText(gr.toString())
+            }
         }
     }.start(wait = true)
 
 }
-*/
